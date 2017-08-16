f81ee7f6eca2b60ebceefeca08ccd3ffbae163a5
carborn <carborn@163.com>
2017-08-16 10:28:20 +0800

    create a program
    firt commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hellogit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello git");
+            Console.ReadLine();
+        }
+    }
+}
